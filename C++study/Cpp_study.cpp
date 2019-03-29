6c705c6216d4eb1ea634b0bcf8020dcd2cb7f7b5
LBW <1159124378@qq.com>
2019-03-29 22:26:48 +0800

    2048 game run in vs2017

---
+++ C++study/Cpp_study.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+
+int main(void)
+{
+    using namespace std;
+    char str_char;
+    string str_string;
+    cin >> str_char;
+    cout<<"you have got char"<<endl;
+    cin >> str_string;
+    cout<< str_string<<endl;
+    return 0;
+}
